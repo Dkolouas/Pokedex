c13747e81cb2c1d5b582be0d83191491216312d5
Dkolouas <dkolouas@gmail.com>
2021-12-26 14:03:46 -0500

    added variables
    added pokemon list 1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,16 @@
-// let myName = 'Bob';
-// document.write(myName);
-// myName = 'John Doe';
-// document.write(myName);
-// myAge = '35'
-// document.write(myAge);
-// alert(myName);
-
-// let simpleAddition = 2 + 2;
-// document.write(simpleAddition);
-// let size = 100;
-// let doubleSize = size * 2;
-// document.write(doubleSize);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// let minSize = (doubleSize * 2) - (size / 2);
-// document.write(minSize);
-
-/*alert() makes pop ups
-console.log()writes in console log :P
-document.write()writes in document */
-
-alert('Hello world')
-let favoriteFood = 'Pasta'
-document.write(favoriteFood)
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    type: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: ['water']
+  }
+];
