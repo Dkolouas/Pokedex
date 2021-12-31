529304e6cc2dddba6e1f7dfcc8d15b5964b273d1
Dkolouas <dkolouas@gmail.com>
2021-12-30 19:30:06 -0500

    1.3, Implemented loop
    added 1 more pokemon as well

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,22 @@ let pokemonList = [{
     name: 'Squirtle',
     height: 0.5,
     type: ['water']
+  },
+  {
+    name: 'Arbok',
+    height: 3.5,
+    type: ['Poison']
   }
 ];
+// this loop allows the atributes to display on the DOM
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' +
+    pokemonList[i].height + ', type: ' + pokemonList[i].type + ')')
+  // this adds a conditional for the height
+  if (pokemonList[i].height > 1.5) {
+    document.write(' Wow, that’s big!')
+  }
+  // adds a break after each loop
+  document.write('<br>')
+
+}
